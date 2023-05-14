9ebbdc1cb8197e294b6be7a9b6a6534dee54b077
Ismail-ahmadi <ahmadi.ismail92@gmail.com>
2023-05-14 13:25:54 -0700

    skills + portfolio

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { faHouse, faImage, faCamera } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
+  faHouse =faHouse;
+  faImage =faImage;
+  faCamera =faCamera;
 
 }
