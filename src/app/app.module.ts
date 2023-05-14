21aa1d6657b6f09bb125b96119f06d19727ddaac
Ismail-ahmadi <ahmadi.ismail92@gmail.com>
2023-05-13 19:23:44 -0700

    add all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { ReferencesComponent } from './components/references/references.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CtaComponent } from './reusable/cta/cta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    SocialMediaComponent,
+    IntroductionComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ReferencesComponent,
+    ContactComponent,
+    FooterComponent,
+    CtaComponent
   ],
   imports: [
     BrowserModule
